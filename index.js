62ac3b6647af50d1bec6bbc767f2f798d3cc8015
Prabakaran <prabakaran271202@gmail.com>
2023-02-27 21:20:52 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,42 @@
+//require necessary modules
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+//require routes
+
+//set up express app and middleware
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.method, req.path, new Date().toLocaleString());
+  next();
+});
+
+//define routes
+
+//production mode
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/dist"));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
+
+//connect to mongoDB and start server
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(
+    "mongodb+srv://prabakaran:Praba%4012345@cluster0.bu3ce.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to Mongo DB");
+    app.listen(process.env.PORT, () => {
+      console.log("server listening on port " + process.env.PORT);
+    });
+  });
